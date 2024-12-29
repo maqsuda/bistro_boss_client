9f3f18fbd951d338abe5f1df30a13ad3c78f3411
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2024-12-29 22:25:08 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { router } from './Routes/Routes'
+import { RouterProvider } from 'react-router-dom'
+
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div className='max-w-screen-xl mx-auto'>
+      <RouterProvider router={router} />
+    </div>
+  </StrictMode>,
+)
